729bf6d5f11009afb98a44358dc5e81705418b87
Arturstm <96059914+Arturstm@users.noreply.github.com>
2022-05-22 01:25:58 +0400

    feat: add 04 code

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,19 @@
-
+const fs = require('fs');
+const path = require('path');
+const {stdin,stdout} = require('process');
+fs.mkdir(path.join(__dirname, 'files-copy'), {
+    recursive: true
+}, err => {
+    if (err) {
+        console.error(err)
+    }
+    fs.readdir(path.join(__dirname, 'files'), (err, filenames) => {
+        if (err) {
+            console.error(err);
+        }
+        for (let filename of filenames) {
+            fs.promises.copyFile(path.join(__dirname, 'files', filename), path.join(__dirname, 'files-copy', filename));
+        }
+    });
+    stdout.write('Good!');
+})
